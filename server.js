ebcd064e2d8b7967422f47950a611f6f33623b0a
Magdi Tiea <magditiea@gmail.com>
2021-04-24 13:40:55 -0400

    server.js files created

---
+++ server.js
@@ -0,0 +1,6 @@
+const { urlencoded } = require('express');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
--- src/components/card.js
+++ src/components/card.js
@@ -3,7 +3,10 @@ import "./style.css";
 
 function Card() {
   return (
+
+    
 <div class="wrap">
+  <h2>Google Books Search</h2>
    <div class="search">
       <input type="text" class="searchTerm" placeholder="What are you looking for?"/>
       <button type="submit" class="searchButton">
