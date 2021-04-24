9306745cf9a0b813f5034a2f2dfb676da51c90f6
Magdi Tiea <magditiea@gmail.com>
2021-04-24 09:39:03 -0400

    rendering issue resolved

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import Card from "./components/card"
+
+
+
+function App()  {
+  return (
+    <Card />
+  )
+};
+
+export default App;
---
+++ src/components/card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Card() {
+  return (
+    <div id="card">
+      <input id="input"/>
+      <button id="button">Submit</button>
+    </div>
+  )
+}
+
+export default Card;
