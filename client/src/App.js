22c998e45d0077bf20e1b41978940f971e7729a9
Magdi Tiea <magditiea@gmail.com>
2021-05-04 01:47:49 -0400

    book seed collection added to seedDB

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,17 @@
 import './App.css';
 import Header from "./components/header/head"
 import React from 'react';
-
+import Home from "./components/pages/books"
 
 
 function App() {
   return(
+   <div> 
+     
     <Header />
+    <Home />
+
+    </div>
   )
 }
 
--- client/src/components/pages/books.js
+++ client/src/components/pages/books.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Header from "../header/head";
 import { Input, TextArea, FormBtn } from "../form/searchform";
-import { useState } from "react";
+import { Column, Container, Row } from "../grid/index";
+import { useEffect, useState } from "react";
 import API from "../utils/API";
+import Jumbotron from "../jumbotron/jumbotron";
 import { SaveBtn, AllSaved } from "../buttons/saveBtns";
 
 
 
 function Home() {
    
+    // Setting our component's initial state
     const [books, setBooks] = useState({
       title: "",
       author: "",
@@ -17,9 +20,35 @@ function Home() {
     });
 
 
-     function SaveBook() {
+      // Load all books and store them with setBooks
+      // The Effect Hook lets you perform side effects in function components:
 
-     }
+        useEffect(() => {
+          loadBooks()
+        }, [])
+
+
+          // Loads all books and sets them to books
+        function loadBooks() {
+          API.getBooks()
+          .then(res =>
+            setBooks(res.data)
+            ).catch(err => console.log(err));
+        };
+
+
+  // Deletes a book from the database with a given id, then reloads books from the db
+  function deleteBook(id) {
+    API.deleteBook(id)
+    .then( res => loadBooks)
+    .cath(err => console.log(err));
+  }
+
+    // Handles updating component state when the user types into the input field
+
+
+
+
 
 
 
@@ -34,7 +63,39 @@ function Home() {
 
 
     return(
+      <Container fluid>
+        <Row>
+          <Column size="md-6">
+            <Jumbotron>
+              <h1>HELOOO WORLD</h1>
+            </Jumbotron>
+            <form>
+              <Input
+              onChange={} name="title" placeholder="Title (required)" value={}
+              />
+              
+            </form>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          </Column>
+        </Row>
+      </Container>
     )
   
 }
+
+export default Home;
